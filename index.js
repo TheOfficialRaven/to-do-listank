ca33320c7d8d7b7c71fbde3d6a3e963b373797fa
Mark Gór <sath_@outlook.hu>
2025-06-17 23:07:18 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5945,3 +5945,16 @@ function stopSnoozeMonitoring() {
     console.log('Snooze monitoring stopped');
   }
 }
+
+useEffect(() => {
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.getRegistrations().then(registrations => {
+      registrations.forEach(registration => {
+        registration.unregister();
+        caches.keys().then(keys => {
+          keys.forEach(key => caches.delete(key));
+        });
+      });
+    });
+  }
+}, []);
