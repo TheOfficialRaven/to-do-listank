dd9023d2c1412e9bce88714450da9ac250c831dc
Mark Gór <sath_@outlook.hu>
2025-06-18 07:01:26 +0200

    javítás 3

--- index.js
+++ index.js
@@ -5952,15 +5952,3 @@ function stopSnoozeMonitoring() {
   }
 }
 
-useEffect(() => {
-  if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.getRegistrations().then(registrations => {
-      registrations.forEach(registration => {
-        registration.unregister();
-        caches.keys().then(keys => {
-          keys.forEach(key => caches.delete(key));
-        });
-      });
-    });
-  }
-}, []);
